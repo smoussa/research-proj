5910a544c2d1456cae95f6c8427953620480749b
Samir Moussa <sm28g11@soton.ac.uk>
2015-05-01 20:06:31 +0100

    added test image and updated irp test files

--- irp_setup.m
+++ irp_setup.m
@@ -2,13 +2,13 @@
 run matconvnet/matlab/vl_setupnn
 
 % download a pre−trained CNN from the web
-urlwrite ( ...
-'http://www.vlfeat.org/sandbox−matconvnet/models/imagenet−vgg−f.mat', ...
-'imagenet−vgg−f.mat');
-net = load('imagenet−vgg−f.mat');
+% urlwrite ( ...
+% 'http://www.vlfeat.org/sandbox−matconvnet/models/imagenet−vgg−f.mat', ...
+% 'imagenet−vgg−f.mat');
+net = load('imagenet-vgg-s.mat');
 
 % obtain and preprocess an image
 im 	= imread('peppers.png');
 im_ = single(im); % note: 255 range
 im_ = imresize(im_, net.normalization.imageSize(1:2));
-im_ = im_ − net.normalization.averageImage;
+im_ = im_ - net.normalization.averageImage;
